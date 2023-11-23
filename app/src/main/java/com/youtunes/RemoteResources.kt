ba0908379818f70aa5eed5f1e5df69af6aa929e5
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2023-11-23 16:45:48 +0100

    Modification de RemoteResources.kt pour éviter d'utiliser un thread, ce qui causait un problème.

--- app/src/main/java/com/youtunes/RemoteResources.kt
+++ app/src/main/java/com/youtunes/RemoteResources.kt
@@ -1,31 +1,42 @@
 package com.youtunes
 
-import android.os.AsyncTask
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.IOException
+import androidx.lifecycle.lifecycleScope
+
 
 class RemoteResources: AppCompatActivity() {
     data class Artist(val name: String)
     data class Event(val name: String)
 
-    fun getFavouriteArtists(limit: Int): List<Artist> {
-        val a = AsyncSpotifyCall().get()
-        return ArrayList()
+    fun getFavouriteArtists(limit: Int=10): List<Artist> {
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val request = Request.Builder()
+                    .url("https://api.spotify.com/v1/me/top/artists")
+                    .addHeader("Authorization", "Bearer " + SpotifyKeyStorage.getBearer())
+                    .build()
+
+                val result = OkHttpClient().newCall(request).execute()
+                val responseBody = result.body?.string()
+
+                // Faire quelque chose avec la réponse ici (par exemple, afficher dans le log)
+                Log.w(TAG, responseBody.toString())
+            } catch (e: IOException) {
+                // Gérer les erreurs ici
+                e.printStackTrace()
+            }
+        }
+        return ArrayList();
     }
 
     fun getArtistEvents(artist: Artist, limit: Int): List<Event> {
         return ArrayList<Event>()
-    }
-
-    private inner class AsyncSpotifyCall: AsyncTask<Void, Void, String>() {
-        override fun doInBackground(vararg params: Void?): String {
-            val request = Request.Builder()
-                .url("https://api.spotify.com/v1/me/top/artists")
-                .addHeader("Authorization", "Bearer " + SpotifyKeyStorage.getBearer())
-                .build()
-            val result = OkHttpClient().newCall(request).execute()
-            return result.body?.string() ?: ""
-        }
     }
 }
