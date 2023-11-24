0688b7de5ac582e4cebffa098b7b8bb29d9558d5
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2023-11-24 14:40:42 +0100

    Updating threads handling Usage of Spotify API refresh token

--- app/src/main/java/com/youtunes/LoginActivity.kt
+++ app/src/main/java/com/youtunes/LoginActivity.kt
@@ -3,27 +3,34 @@ package com.youtunes
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.security.SecureRandom
 import okhttp3.HttpUrl
+import java.io.File
 
-class LoginActivity :AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private val client_id = BuildConfig.Spotify_client_id;
     private val redirect_uri = "com.youtune://spotify-callback";
     private val scope = "user-top-read";
+    private lateinit var fileDir: File;
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        if (ChecksIfTheUseHasASecretId()) {
-            getNewSpotifyKeyBySecretId();
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        fileDir = this.filesDir
+        if (ChecksIfTheUseHasASecretId(fileDir)) {
+            val intent = Intent(this@LoginActivity, SpotifyCallback::class.java)
+            startActivity(intent)
+            finish()
         } else {
+            Log.w(TAG, "On passe ici ")
             callSpotifyAPIForGetSpotifyKey()
         }
-
     }
 
     /**
@@ -33,42 +40,47 @@ class LoginActivity :AppCompatActivity() {
      * au lieu de refaire toute les demande ce qui est lourd
      * @return false si l'utilisateur c'est jamais connecter, true dans le cas inverse
      */
-    fun ChecksIfTheUseHasASecretId(): Boolean {
-        return false
+    fun ChecksIfTheUseHasASecretId(fileDir: File): Boolean {
+        return SpotifyKeyStorage.getRefrecher(fileDir) != ""
     }
 
     /**
      * Fonction à implementer
      */
-    fun getNewSpotifyKeyBySecretId() {
+    private fun getNewSpotifyKeyBySecretId() {
         throw NotImplementedError("Methode non implémenter");
     }
 
-    fun callSpotifyAPIForGetSpotifyKey() {
-        //on prépare la requete
-        var state = generateRandomString(16);
-        val authorizeUrl = HttpUrl.Builder()
-            .scheme("https")
-            .host("accounts.spotify.com")
-            .addPathSegments("authorize")
-            .addQueryParameter("response_type", "code")
-            .addQueryParameter("client_id", client_id)
-            .addQueryParameter("scope", scope)
-            .addQueryParameter("redirect_uri", redirect_uri)
-            .addQueryParameter("state", state)
-            .build()
-        //on exécute la requete
-        RedirectAsyncTask().execute(authorizeUrl.toString())
-
+    /**
+     * Permet d'appeler l'API de spotify pour récupérer les autorization nécessaire
+     * pour le fonctionnement de l'application
+     */
+    private fun callSpotifyAPIForGetSpotifyKey() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            //on prépare la requete
+            var state = generateRandomString(16);
+            val authorizeUrl = HttpUrl.Builder()
+                .scheme("https")
+                .host("accounts.spotify.com")
+                .addPathSegments("authorize")
+                .addQueryParameter("response_type", "code")
+                .addQueryParameter("client_id", client_id)
+                .addQueryParameter("scope", scope)
+                .addQueryParameter("redirect_uri", redirect_uri)
+                .addQueryParameter("state", state)
+                .build()
+            redirectAction(authorizeUrl)
+        }
     }
 
     /**
-     * fonction qui permet de générer une suite de charactère aléatoire
+     * fonction qui permet de générer une suite de caractère aléatoire
      * info pas obligatoire mais très recommandé par spotify car
      * This provides protection against attacks such as cross-site request forgery.
      * Plus d'info ici : https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+     * @param length : Longueur de la chaine de caractère
      */
-    fun generateRandomString(length: Int): String {
+    private fun generateRandomString(length: Int): String {
         val characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         val random = SecureRandom()
         val sb = StringBuilder(length)
@@ -77,27 +89,20 @@ class LoginActivity :AppCompatActivity() {
             val randomChar = characters[random.nextInt(characters.length)]
             sb.append(randomChar)
         }
-
         return sb.toString()
     }
 
     /**
-     * Class qui permet de faire l'appel API sur un thread séparer
-     * (et donc impacte pas l'application)
-     * AsyncTask est deprecated donc voir si pas moyen de faire autrement
-     * Voir si aussi pas moyen de faire ceci dans une class public pour pouvoir
-     * l'appeler à plusieurs endroit
-     * (vus qu'on bouge après sur la page
+     * Permet de rediriger vers la page
+     * @param authorizeUrl : lien de la page web de spotify pour s'authentifier
      */
-    private inner class RedirectAsyncTask : AsyncTask<String, Void, Unit>() {
-        override fun doInBackground(vararg params: String?) {
-            val redirectUrl = params[0]
-            val intent = Intent(Intent.ACTION_VIEW)
-            val uri = Uri.parse(redirectUrl)
-            Log.w(TAG,"URL")
-            Log.w(TAG,uri.toString())
-            intent.data = uri
-            startActivity(intent)
-        }
+    private fun redirectAction(authorizeUrl: HttpUrl) {
+        val redirectUrl = authorizeUrl.toString()
+        val intent = Intent(Intent.ACTION_VIEW)
+        val uri = Uri.parse(redirectUrl)
+        Log.w(TAG, "URL")
+        Log.w(TAG, uri.toString())
+        intent.data = uri
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/youtunes/YourSummaryList.kt
+++ app/src/main/java/com/youtunes/YourSummaryList.kt
@@ -2,13 +2,14 @@ package com.youtunes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
 
-class YourSummaryListActivity: AppCompatActivity() {
-
+class YourSummaryListActivity : AppCompatActivity() {
+    private lateinit var fileDir: File;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_summary);
-        RemoteResources().getFavouriteArtists(1)
+        fileDir = this.filesDir
+        RemoteResources().getFavouriteArtists(1, fileDir)
     }
-
 }
