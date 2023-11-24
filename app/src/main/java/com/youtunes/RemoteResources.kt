b854d731f0fc96082a197b38814bb30d5566e50e
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2023-11-24 14:39:40 +0100

    Modification de RemoteResources.kt pour éviter d'utiliser un thread, ce qui causait un problème.

--- app/src/main/java/com/youtunes/RemoteResources.kt
+++ app/src/main/java/com/youtunes/RemoteResources.kt
@@ -7,20 +7,20 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.IOException
 import androidx.lifecycle.lifecycleScope
+import java.io.File
 
 
 class RemoteResources: AppCompatActivity() {
     data class Artist(val name: String)
     data class Event(val name: String)
 
-    fun getFavouriteArtists(limit: Int=10): List<Artist> {
+     fun getFavouriteArtists(limit: Int = 10, fileDir: File): List<Artist> {
         lifecycleScope.launch(Dispatchers.IO) {
             try {
                 val request = Request.Builder()
                     .url("https://api.spotify.com/v1/me/top/artists")
-                    .addHeader("Authorization", "Bearer " + SpotifyKeyStorage.getBearer())
+                    .addHeader("Authorization", "Bearer " + SpotifyKeyStorage.getBearer(fileDir))
                     .build()
 
                 val result = OkHttpClient().newCall(request).execute()
@@ -28,13 +28,14 @@ class RemoteResources: AppCompatActivity() {
 
                 // Faire quelque chose avec la réponse ici (par exemple, afficher dans le log)
                 Log.w(TAG, responseBody.toString())
-            } catch (e: IOException) {
-                // Gérer les erreurs ici
-                e.printStackTrace()
+
+            }catch (error:Error){
+
             }
         }
-        return ArrayList();
+        return ArrayList()
     }
+
 
     fun getArtistEvents(artist: Artist, limit: Int): List<Event> {
         return ArrayList<Event>()
