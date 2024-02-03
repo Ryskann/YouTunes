7867a46ec3a253a923ad881673800db0e73d8a98
Ryskann <93831988+Ryskann@users.noreply.github.com>
2024-02-03 01:42:31 +0100

    Added Ticket Master Button

--- app/src/main/java/com/youtunes/EventItemAdapter.kt
+++ app/src/main/java/com/youtunes/EventItemAdapter.kt
@@ -1,30 +1,22 @@
 package com.youtunes
 
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import java.io.File
-import java.net.URI
 
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 
 class EventItemAdapter(private val context: Context,private val dataset:List<Event>) : RecyclerView.Adapter<EventItemAdapter.EventItemHolder>(){
     class EventItemHolder(private val view: View): RecyclerView.ViewHolder(view){
         val eventPlace :TextView=view.findViewById(R.id.eventPlace)
         val eventCity: TextView=view.findViewById(R.id.eventCity)
         val eventDate: TextView=view.findViewById(R.id.eventDate)
+        val TMButton: Button = view.findViewById(R.id.TM_button)
     }
 
     override fun onCreateViewHolder(
@@ -40,6 +32,13 @@ class EventItemAdapter(private val context: Context,private val dataset:List<Eve
         holder.eventPlace.text = item.eventPlace
         holder.eventCity.text = item.eventCity
         holder.eventDate.text = item.eventDate.dayOfMonth.toString() + "/" + item.eventDate.month.toString() + "/" + item.eventDate.year.toString()
+
+        holder.TMButton.setOnClickListener{
+            val url = item.ticketMasterUrl
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
